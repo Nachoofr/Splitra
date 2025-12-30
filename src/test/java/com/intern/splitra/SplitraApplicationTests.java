103d9b8d93f130f91de7bd453d9e362adce121a2
nachoofr <sachhyamnyachhyon@gmail.com>
2025-12-30 17:16:34 +0545

    Setup

---
+++ src/test/java/com/intern/splitra/SplitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.splitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
