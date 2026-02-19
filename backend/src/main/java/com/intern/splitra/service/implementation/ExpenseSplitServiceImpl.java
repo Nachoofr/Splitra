aa14b615bab42d8384526c94a2cdd43be7d4fe12
nachoofr <sachhyamnyachhyon@gmail.com>
2026-02-19 12:34:49 +0545

    SPLIT-78 Backend logic for Percentage wise split completed

--- backend/src/main/java/com/intern/splitra/service/implementation/ExpenseSplitServiceImpl.java
+++ backend/src/main/java/com/intern/splitra/service/implementation/ExpenseSplitServiceImpl.java
@@ -18,7 +18,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -80,15 +82,67 @@ public class ExpenseSplitServiceImpl implements ExpenseSplitService {
         return new ResponseEntity<>(splitDto, HttpStatus.OK);
     }
 
+
     public ResponseEntity<List<ExpenseSplitDto>> splitExpense(Expense expense, ExpenseSplitRequestDto expenseSplitRequestDto, User user){
         var splitMethod = expense.getSplitMethod();
         return switch (splitMethod) {
             case EQUALLY -> equalSplit(expense.getId(), expenseSplitRequestDto.getEqualSplitId(), user.getId());
+            case PERCENTWISE ->  percentageSplit(expense.getId(), expenseSplitRequestDto.getPercentageSplitId(), user.getId());
             default -> throw new RuntimeException("Invalid splitMethod");
-//            case PERCENTWISE ->
 //            case ITEMWISE ->
         };
+    }
 
+
+    public ResponseEntity<List<ExpenseSplitDto>> percentageSplit(long expenseId, Map<Long, Double > userIds, long loggedInUserId) {
+        if(userIds.size() < 2){
+            throw new RuntimeException("At least two users are required for splitting the expense");
+        }
+
+        Expense expense = expenseRepo.findById(expenseId)
+                .orElseThrow(() -> new RuntimeException("Expense not found"));
+
+        Groups group = expense.getGroup();
+
+        if (!group.getMembers().stream().anyMatch(member -> member.getId() == loggedInUserId)){
+            throw new RuntimeException("User is not a member of the group");
+        }
+
+        for (long userId : userIds.keySet()) {
+            if (!group.getMembers().stream().anyMatch(member -> member.getId() == userId)){
+                throw new RuntimeException("User of userId "+ userId + " is not a member of the group");
+            }
+        }
+
+        double totalPercentage = userIds.values().stream().mapToDouble(Double::doubleValue).sum();
+        if (totalPercentage != 100){
+            throw new RuntimeException("Total percentage of users are not equal to 100");
+        }
+
+        List<ExpenseSplit> splitData = new ArrayList<>();
+
+        for  (Map.Entry<Long, Double> userId : userIds.entrySet()) {
+            User  user = userRepo.findById(userId.getKey())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            double splitAmount = (userId.getValue()/100) * expense.getAmount();
+
+            ExpenseSplit expenseSplit = new ExpenseSplit();
+            expenseSplit.setExpense(expense);
+            expenseSplit.setUserId(user);
+            expenseSplit.setAmount(splitAmount);
+            splitData.add(expenseSplit);
+        }
+
+        expenseSplitRepo.deleteAllByExpenseId(expenseId);
+
+        expenseSplitRepo.saveAll(splitData);
+
+        List<ExpenseSplitDto> splitDto = splitData.stream()
+                .map(expenseSplitMapper::toDto)
+                .toList();
+
+        return new ResponseEntity<>(splitDto, HttpStatus.OK);
     }
 
 
